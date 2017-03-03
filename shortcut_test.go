71f2e56d7c39e15a12b13a6d11c4b0830f8f97d9
joesis <joesis@getlantern.org>
2017-03-03 11:01:25 +0800

    add ipv6 address of google public dns

--- shortcut_test.go
+++ shortcut_test.go
@@ -10,7 +10,7 @@ import (
 func TestAllow(t *testing.T) {
 	s := NewFromReader(
 		strings.NewReader("127.0.0.0/24\n8.8.0.0/16\n"),
-		strings.NewReader("fe80::1/64\n::/64\n"),
+		strings.NewReader("fe80::1/64\n::/64\n2001:4860:4860::8800/120\n"),
 	)
 	assert.True(t, s.Allow("127.0.0.1:8888"))
 	assert.True(t, s.Allow("localhost:8888"))
